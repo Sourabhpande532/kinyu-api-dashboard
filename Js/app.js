6e0a7ad5a9db6ba57df4c325d744e910ca35edc8
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2025-08-05 21:58:54 +0530

    feat: add table data

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,4 @@
 import router from "./route.js";
-
+import lendingAPIs from "./api/lendings.mock.server.js"
 router.start();
+lendingAPIs();
--- Js/pages/home.js
+++ Js/pages/home.js
@@ -1,8 +1,35 @@
+import { apiGetLendings } from "../api/apiGetLendings.js";
 
-function tableListData() {
-    return `<h1>Table zone</h1>`
+
+const renderData = ()=>{
+    // 
 }
 
-export default function render() {
-    document.getElementById( "app" ).innerHTML = tableListData();
+function LendingData( lending ) {
+    const renderTableData = lending.map( renderData );
+    const rowHtmlData = renderTableData.join( "" );
+    return `<table>
+        <thead>
+            <tr>
+                <th>Who</th>
+                <th>Type</th>
+                <th>Details</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+    </table>`
+}
+
+function ErrorBanner( error ) {
+    return `<hgroup>
+        <h2>Error Occured ${ error }</h2>
+        </hgroup>`
+}
+export default async function render() {
+    const { error, data } = await apiGetLendings();
+    if ( error ) {
+        document.getElementById( "app" ).innerHTML = ErrorBanner( error )
+        return;
+    }
+    document.getElementById( "app" ).innerHTML = LendingData( data )
 }
