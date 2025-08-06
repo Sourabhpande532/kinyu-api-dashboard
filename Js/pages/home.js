fa1563dcb6dc7fa796563bedcee049b273c59be6
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2025-08-06 20:16:38 +0530

    feat: add containt through api

--- Js/pages/home.js
+++ Js/pages/home.js
@@ -1,9 +1,32 @@
 import { apiGetLendings } from "../api/apiGetLendings.js";
 
+const renderTableData = ( eachData ) => {
+    const { id, who, type, details } = eachData;
+    return `<tr>
+        <td>${ who }</td>
+        <td>${ type }</td>
+        <td>${ details }</td>
+        <td><a href="details${ id }">View</a></td>
+    </tr>`
+}
 
 const LendingData = ( lending ) => {
     const lendingData = lending.map( renderTableData );
     const appendRowsHtml = lendingData.join( "" );
+    return `<table>
+        <thead>
+            <tr>
+                <th>Who</th>
+                <th>Type</th>
+                <th>Details</th>
+                <th>View</th>
+            </tr>
+        </thead>
+        <tbody>
+         ${ appendRowsHtml }
+        </tbody>
+    </table>`
+
 }
 
 function ErrorBanner( error ) {
