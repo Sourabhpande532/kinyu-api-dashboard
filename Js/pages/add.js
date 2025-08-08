62b65dc03419496e8d68539f5b26da19771949a5
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2025-08-08 22:39:48 +0530

    ADD: details page

--- Js/pages/add.js
+++ Js/pages/add.js
@@ -1,14 +1,25 @@
-
-const AddForms = ()=>{
-    return `<div>
+const AddForms = () => {
+  return `
     <h2>What did you lend recently</h2>
     <hr/>
     <form>
     <input type="text" id="toWhom" placeholder="who"/>
+    <select id="selectedCategory">
+    <option value="" selected disabled>---Select---</option>
+    <option>Book</option>
+    <option>Money</option>
+    <option>Tool</option>
+    <option>Other</option>
+    </select>
+    <input type="datetime-local" id="loan_date"/>
+    <input type="text" placeholder="Additional Details"
+    id="details_loan"/>
+    <button type="submit">Add Lendings</button>
     </form>
-    </div>`
-}
+    <p id="result"></p>
+    `;
+};
 
-export default function add(){
-document.getElementById("app").innerHTML = AddForms()
+export default function add() {
+  document.getElementById("app").innerHTML = AddForms();
 }
